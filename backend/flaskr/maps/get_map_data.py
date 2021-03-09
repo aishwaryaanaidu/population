e85f24fbcdc00e5e0ba9b94138ed2ecd8ae08985
Aishwarya Naidu <aishwarya13naidu@gmail.com>
2021-03-09 20:57:40 +0530

    Adding try catch block

--- backend/flaskr/maps/get_map_data.py
+++ backend/flaskr/maps/get_map_data.py
@@ -4,4 +4,13 @@ from flask_restful import Resource
 
 class GetMapData(Resource):
     def get(self):
-        return "Hello"
+        try:
+            return "Hello"
+        except (ValueError, KeyError, TypeError) as exception:
+            return {
+                'message': 'Unexpected'
+            }, 400
+        except Exception as exception:
+            return {
+                'message': 'Internal server error'
+            }, 500
