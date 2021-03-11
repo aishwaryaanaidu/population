3320061cb47b149c705874d2f018d4dcd3a8e774
Aishwarya Naidu <aishwarya13naidu@gmail.com>
2021-03-11 21:18:21 +0530

    Parsing logic

--- backend/flaskr/maps/get_map_data.py
+++ backend/flaskr/maps/get_map_data.py
@@ -1,11 +1,18 @@
 from flask import request
+import requests
 from flask_restful import Resource
-
+from bs4 import BeautifulSoup
 
 class GetMapData(Resource):
     def get(self):
         try:
-            return "Hello"
+            page = requests.get("https://www.worldometers.info/world-population/population-by-country/")
+            soup = BeautifulSoup(page.text, 'html.parser')
+            # returns a list of tables present on the page. Choose the one you need
+            table = soup.find_all('table')[0]
+            for child in table.children:
+                for td in child:
+                    print(td)
         except (ValueError, KeyError, TypeError) as exception:
             return {
                 'message': 'Unexpected'
