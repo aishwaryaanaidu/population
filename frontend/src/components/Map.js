1eb6bb4ec1d91cae5992517ec339cc991f6870d0
Aishwarya M S <38344621+aishwaryaanaidu@users.noreply.github.com>
2021-04-01 21:27:07 +0530

    Adjusting map size to fit full screen

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -12,18 +12,20 @@ export default function Map() {
     }, [])
 
     return (
-        <Chart
-            // width={'500px'}
-            // height={'300px'}
-            chartType="GeoChart"
-            data={mapsData}
-            mapsApiKey="AIzaSyAtpxG25mQR4mrbRH__lYtMvwJcRcB-lEs"
-            rootProps={{ 'data-testid': '1' }}
-            options={{
-                colorAxis: { colors: ['#FFA8D3', '#C71C69'] },
-                backgroundColor: '#B8E8F0',
-                datalessRegionColor: '#FFA8D3',
-            }}
-        />
+        <div>
+            <Chart
+                width={'100vw'}
+                height={'89vh'}
+                chartType="GeoChart"
+                data={mapsData}
+                mapsApiKey="AIzaSyAtpxG25mQR4mrbRH__lYtMvwJcRcB-lEs"
+                rootProps={{ 'data-testid': '1' }}
+                options={{
+                    colorAxis: { colors: ['#FFA8D3', '#C71C69'] },
+                    backgroundColor: '#B8E8F0',
+                    datalessRegionColor: '#FFA8D3',
+                }}
+            />
+        </div>
     )
 }
