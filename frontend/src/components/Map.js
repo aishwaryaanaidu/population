5727a921ee6fb4a1f75b99000e1d8a42575ec92f
Aishwarya M S <38344621+aishwaryaanaidu@users.noreply.github.com>
2021-04-01 20:12:45 +0530

    Displaying dynamic data on the map

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Chart } from 'react-google-charts';
 
-export default function Map () {
+export default function Map() {
+    const [mapsData, setMapsData] = useState([]);
+
+    useEffect(() => {
+        return fetch('http://127.0.0.1:5000/maps/get_data')
+            .then(response => response.json())
+            .then(data => setMapsData(data))
+            .catch(error => console.log(error))
+    }, [])
+
     return (
         <Chart
             // width={'500px'}
             // height={'300px'}
             chartType="GeoChart"
-            data={[
-                ['Country', 'Popularity'],
-                ['Germany', 200],
-                ['United States', 300],
-                ['Brazil', 400],
-                ['Canada', 500],
-                ['France', 600],
-                ['RU', 700],
-            ]}
+            data={mapsData}
             mapsApiKey="AIzaSyAtpxG25mQR4mrbRH__lYtMvwJcRcB-lEs"
             rootProps={{ 'data-testid': '1' }}
+            options={{
+                colorAxis: { colors: ['#FFA8D3', '#C71C69'] },
+                backgroundColor: '#B8E8F0',
+                datalessRegionColor: '#FFA8D3',
+            }}
         />
     )
 }
